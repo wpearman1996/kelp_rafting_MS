edcb51f04f12160ba61654c7b00131a21c4fa073
wpearman1996 <38397623+wpearman1996@users.noreply.github.com>
2023-12-04 10:10:36 +1300

    Add files via upload

---
+++ elbow_core_analysis.R
@@ -0,0 +1,220 @@
+create_core_comm<-function(physeq,nReads){
+  library(tidyverse)
+  library(reshape2)
+  library(vegan)
+  library(ggsci)
+  library(phyloseq)
+    theme_set(theme_light())
+  nReads=nReads
+otu<-(physeq@otu_table@.Data)
+otu_PA <- 1*((otu>0)==1)                                               # presence-absence data
+otu_occ <- rowSums(otu_PA)/ncol(otu_PA)                                # occupancy calculation
+otu_rel <- apply(decostand(otu, method="total", MARGIN=2),1, mean)     # relative abundance  
+occ_abun <- data.frame(otu_occ=otu_occ, otu_rel=otu_rel) %>%           # combining occupancy and abundance data frame
+  rownames_to_column('otu')
+map<-sample_data(physeq)
+
+PresenceSum <- data.frame(otu = as.factor(row.names(otu)), otu) %>% 
+  gather(Sample, abun, -otu) %>%
+  dplyr::left_join(map, by = 'Sample') %>%
+  group_by(otu, Location) %>%
+  summarise(plot_freq=sum(abun>0)/length(abun),        # frequency of detection between time points
+            coreSite=ifelse(plot_freq == 1, 1, 0), # 1 only if occupancy 1 with specific genotype, 0 if not
+            detect=ifelse(plot_freq > 0, 1, 0)) %>%    # 1 if detected and 0 if not detected with specific genotype
+  group_by(otu) %>%
+  dplyr::summarise(sumF=sum(plot_freq),
+            sumG=sum(coreSite),
+            nS=length(Location)*2,
+            Index=(sumF+sumG)/nS) # calculating weighting Index based on number of time points detected and 
+
+otu_ranked <- occ_abun %>%
+  dplyr::left_join(PresenceSum, by='otu') %>%
+  transmute(otu=otu,
+            rank=Index) %>%
+  arrange(desc(rank))
+
+BCaddition <- NULL
+
+otu_start=otu_ranked$otu[1]
+start_matrix <- as.matrix(otu[otu_start,])
+start_matrix <- t(start_matrix)
+x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))
+x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+df_s <- data.frame(x_names,x)
+names(df_s)[2] <- 1 
+BCaddition <- rbind(BCaddition,df_s)
+
+for(i in 2:200){
+  otu_add=otu_ranked$otu[i]
+  add_matrix <- as.matrix(otu[otu_add,])
+  add_matrix <- t(add_matrix)
+  start_matrix <- rbind(start_matrix, add_matrix)
+  x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]-start_matrix[,x[2]]))/(2*nReads))
+  x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+  df_a <- data.frame(x_names,x)
+  names(df_a)[2] <- i 
+  BCaddition <- dplyr::left_join(BCaddition, df_a, by=c('x_names'))  
+}
+
+x <-  apply(combn(ncol(otu), 2), 2, function(x) sum(abs(otu[,x[1]]-otu[,x[2]]))/(2*nReads))
+x_names <- apply(combn(ncol(otu), 2), 2, function(x) paste(colnames(otu)[x], collapse=' - '))
+df_full <- data.frame(x_names,x)
+names(df_full)[2] <- length(rownames(otu))
+BCfull <- dplyr::left_join(BCaddition,df_full, by='x_names')
+
+rownames(BCfull) <- BCfull$x_names
+temp_BC <- BCfull
+temp_BC$x_names <- NULL
+temp_BC_matrix <- as.matrix(temp_BC)
+
+BC_ranked <- data.frame(rank = as.factor(row.names(t(temp_BC_matrix))),t(temp_BC_matrix)) %>% 
+  gather(comparison, BC, -rank) %>%
+  group_by(rank) %>%
+  dplyr::summarise(MeanBC=mean(BC)) %>%
+  arrange(-desc(MeanBC)) %>%
+  mutate(proportionBC=MeanBC/max(MeanBC))
+Increase=BC_ranked$MeanBC[-1]/BC_ranked$MeanBC[-length(BC_ranked$MeanBC)]
+increaseDF <- data.frame(IncreaseBC=c(0,(Increase)), rank=factor(c(1:(length(Increase)+1))))
+BC_ranked <- dplyr::left_join(BC_ranked, increaseDF)
+BC_ranked <- BC_ranked[-nrow(BC_ranked),]
+
+fo_difference <- function(pos){
+  left <- (BC_ranked[pos, 2] - BC_ranked[1, 2]) / pos
+  right <- (BC_ranked[nrow(BC_ranked), 2] - BC_ranked[pos, 2]) / (nrow(BC_ranked) - pos)
+  return(left - right)
+}
+BC_ranked$fo_diffs <- sapply(1:nrow(BC_ranked), fo_difference)
+elbow <- which.max(BC_ranked$fo_diffs)
+
+lastCall <- last(as.numeric(as.character(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.02)])))
+
+graph<-ggplot(BC_ranked[1:200,], aes(x=factor(BC_ranked$rank[1:200], levels=BC_ranked$rank[1:200]))) +
+  geom_point(aes(y=proportionBC)) +
+  theme_classic() + theme(strip.background = element_blank(),axis.text.x = element_text(size=7, angle=45)) +
+  geom_vline(xintercept=elbow, lty=3, col='red', cex=.5) +
+  geom_vline(xintercept=lastCall, lty=3, col='blue', cex=.5) +
+  labs(x='ranked OTUs',y='Bray-Curtis similarity') +
+  annotate(geom="text", x=elbow+10, y=.15, label=paste("Elbow method"," (",elbow,")", sep=''), color="red")+    
+  annotate(geom="text", x=lastCall-4, y=.08, label=paste("Last 2% increase (",lastCall,')',sep=''), color="blue")
+
+otu_ranked<-otu_ranked
+last_call<-lastCall
+output<-list(graph,otu_ranked,lastCall,BC_ranked)
+output
+}
+create_core_func_comm<-function(physeq,nReads){
+  library(tidyverse)
+  library(reshape2)
+  library(vegan)
+  library(ggsci)
+  theme_set(theme_light())
+  nReads=nReads
+  otu<-physeq@otu_table@.Data
+  otu_PA <- 1*((otu>0)==1)                                               # presence-absence data
+  otu_occ <- rowSums(otu_PA)/ncol(otu_PA)                                # occupancy calculation
+  otu_rel <- apply(decostand(otu, method="total", MARGIN=2),1, mean)     # relative abundance  
+  occ_abun <- data.frame(otu_occ=otu_occ, otu_rel=otu_rel) %>%           # combining occupancy and abundance data frame
+    rownames_to_column('otu')
+  map<-sample_data(physeq)
+  
+  PresenceSum <- data.frame(otu = as.factor(row.names(otu)), otu) %>% 
+    gather(Sample, abun, -otu) %>%
+    left_join(map, by = 'Sample') %>%
+    group_by(otu, Location) %>%
+    summarise(plot_freq=sum(abun>0)/length(abun),        # frequency of detection between time points
+              coreSite=ifelse(plot_freq == 1, 1, 0), # 1 only if occupancy 1 with specific genotype, 0 if not
+              detect=ifelse(plot_freq > 0, 1, 0)) %>%    # 1 if detected and 0 if not detected with specific genotype
+    group_by(otu) %>%
+    summarise(sumF=sum(plot_freq),
+              sumG=sum(coreSite),
+              nS=length(Location)*2,
+              Index=(sumF+sumG)/nS) # calculating weighting Index based on number of time points detected and 
+  
+  otu_ranked <- occ_abun %>%
+    left_join(PresenceSum, by='otu') %>%
+    transmute(otu=otu,
+              rank=Index) %>%
+    arrange(desc(rank))
+  
+  BCaddition <- NULL
+  
+  otu_start=otu_ranked$otu[1]
+  start_matrix <- as.matrix(otu[otu_start,])
+  start_matrix <- t(start_matrix)
+  x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]- start_matrix[,x[2]]))/(2*nReads))
+  x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+  df_s <- data.frame(x_names,x)
+  names(df_s)[2] <- 1 
+  BCaddition <- rbind(BCaddition,df_s)
+  
+  for(i in 2:30){
+    otu_add=otu_ranked$otu[i]
+    add_matrix <- as.matrix(otu[otu_add,])
+    add_matrix <- t(add_matrix)
+    start_matrix <- rbind(start_matrix, add_matrix)
+    x <- apply(combn(ncol(start_matrix), 2), 2, function(x) sum(abs(start_matrix[,x[1]]-start_matrix[,x[2]]))/(2*nReads))
+    x_names <- apply(combn(ncol(start_matrix), 2), 2, function(x) paste(colnames(start_matrix)[x], collapse=' - '))
+    df_a <- data.frame(x_names,x)
+    names(df_a)[2] <- i 
+    BCaddition <- left_join(BCaddition, df_a, by=c('x_names'))  
+  }
+  
+  x <-  apply(combn(ncol(otu), 2), 2, function(x) sum(abs(otu[,x[1]]-otu[,x[2]]))/(2*nReads))
+  x_names <- apply(combn(ncol(otu), 2), 2, function(x) paste(colnames(otu)[x], collapse=' - '))
+  df_full <- data.frame(x_names,x)
+  names(df_full)[2] <- length(rownames(otu))
+  BCfull <- left_join(BCaddition,df_full, by='x_names')
+  
+  rownames(BCfull) <- BCfull$x_names
+  temp_BC <- BCfull
+  temp_BC$x_names <- NULL
+  temp_BC_matrix <- as.matrix(temp_BC)
+  
+  BC_ranked <- data.frame(rank = as.factor(row.names(t(temp_BC_matrix))),t(temp_BC_matrix)) %>% 
+    gather(comparison, BC, -rank) %>%
+    group_by(rank) %>%
+    summarise(MeanBC=mean(BC)) %>%
+    arrange(-desc(MeanBC)) %>%
+    mutate(proportionBC=MeanBC/max(MeanBC))
+  Increase=BC_ranked$MeanBC[-1]/BC_ranked$MeanBC[-length(BC_ranked$MeanBC)]
+  increaseDF <- data.frame(IncreaseBC=c(0,(Increase)), rank=factor(c(1:(length(Increase)+1))))
+  BC_ranked <- left_join(BC_ranked, increaseDF)
+  BC_ranked <- BC_ranked[-nrow(BC_ranked),]
+  
+  fo_difference <- function(pos){
+    left <- (BC_ranked[pos, 2] - BC_ranked[1, 2]) / pos
+    right <- (BC_ranked[nrow(BC_ranked), 2] - BC_ranked[pos, 2]) / (nrow(BC_ranked) - pos)
+    return(left - right)
+  }
+  BC_ranked$fo_diffs <- sapply(1:nrow(BC_ranked), fo_difference)
+  elbow <- which.max(BC_ranked$fo_diffs)
+  
+  lastCall <- last(as.numeric(as.character(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.03)])))
+  
+  graph<-ggplot(BC_ranked[1:30,], aes(x=factor(BC_ranked$rank[1:30], levels=BC_ranked$rank[1:30]))) +
+    geom_point(aes(y=proportionBC)) +
+    theme_classic() + theme(strip.background = element_blank(),axis.text.x = element_text(size=7, angle=45)) +
+    geom_vline(xintercept=elbow, lty=3, col='red', cex=.5) +
+    geom_vline(xintercept=lastCall, lty=3, col='blue', cex=.5) +
+    labs(x='ranked OTUs',y='Bray-Curtis similarity') +
+    annotate(geom="text", x=elbow+10, y=.15, label=paste("Elbow method"," (",elbow,")", sep=''), color="red")+    
+    annotate(geom="text", x=lastCall-4, y=.08, label=paste("Last 2% increase (",lastCall,')',sep=''), color="blue")
+  
+  otu_ranked<-otu_ranked
+  last_call<-lastCall
+  output<-list(graph,otu_ranked,lastCall)
+  output
+}
+
+core_raft<-create_core_comm(ASV_Munida_Kelp,4000)
+core_inplace<-create_core_comm(ASV_InPlace,4000)
+core_macro[[4]]$MeanBC
+
+ASV_function
+core_func_poha<-create_core_func_comm(ASV_function_poha,3000)
+core_func_ant<-create_core_comm(ASV_function_Ant,3000)
+
+
+core_raft[[1]]
+core_inplace[[2]]$otu[1:10]
+core_raft[[2]]$otu[1:4]
---
+++ ternary_plots.R
@@ -0,0 +1,114 @@
+ASV_InPlace<-readr::read_rds("../Kelp_Rafting_MS/Munida_Analysis/ASV_InPlace.RDS")
+ASV_Munida_Kelp<-readr::read_rds("../Kelp_Rafting_MS/Munida_Analysis/ASV_Munida_Kelp.RDS")
+
+ASV_InPlace_temp<-prune_taxa(taxa_names(ASV_InPlace)[taxa_sums(ASV_InPlace)>0],ASV_InPlace)
+ASV_Munida_Kelp_temp<-prune_taxa(taxa_names(ASV_Munida_Kelp)[taxa_sums(ASV_Munida_Kelp)>0],ASV_Munida_Kelp)
+ASV_InPlace_temp<-prune_taxa(taxa_names(ASV_InPlace_temp)[taxa_sums(ASV_InPlace_temp)>0],ASV_InPlace_temp)
+ASV_InPlace_temp<-prune_samples(ASV_InPlace_temp@sam_data$Sample[!ASV_InPlace_temp@sam_data$Location %in% c("Wainui","Ward","Kaikoura","CapeCampbell","LeBons","Rarangi")],
+                           ASV_InPlace_temp)
+ASV_InPlace_temp<-prune_taxa(taxa_names(ASV_InPlace_temp)[taxa_sums(ASV_InPlace_temp)>0],ASV_InPlace_temp)
+core_inplace<-create_core_comm(ASV_InPlace_temp,4000)
+core_raft<-create_core_comm(ASV_Munida_Kelp_temp,4000)
+
+library(phyloseq)
+ASV_Inplacecore<-prune_taxa(core_inplace[[2]]$otu[1:14],ASV_InPlace_temp)
+
+#core_raft<-create_core_comm(ASV_Munida_Kelp,4000)
+
+BC_ranked<-core_inplace[[4]]
+elbow <- which.max(BC_ranked$fo_diffs)
+
+lastCall <- last(as.numeric(as.character(BC_ranked$rank[(BC_ranked$IncreaseBC>=1.03)])))
+
+graph<-ggplot(BC_ranked[1:200,], aes(x=factor(BC_ranked$rank[1:200], levels=BC_ranked$rank[1:200]))) +
+  geom_point(aes(y=proportionBC)) +
+  theme_classic() + theme(strip.background = element_blank(),axis.text.x = element_text(size=7, angle=45)) +
+  geom_vline(xintercept=elbow, lty=3, col='red', cex=.5) +
+  geom_vline(xintercept=lastCall, lty=3, col='blue', cex=.5) +
+  labs(x='ranked OTUs',y='Bray-Curtis similarity') +
+  annotate(geom="text", x=elbow+10, y=.15, label=paste("Elbow method"," (",elbow,")", sep=''), color="red")+    
+  annotate(geom="text", x=lastCall-4, y=.08, label=paste("Last 2% increase (",lastCall,')',sep=''), color="blue")
+
+
+inplace_core<-core_inplace[[2]]$otu[1:14]
+munida_core<-core_raft[[2]]$otu[1:18]
+
+
+InPlace_Comp <- microbiome::transform(ASV_InPlace, "compositional")
+Munida_Comp <- microbiome::transform(ASV_Munida_Kelp, "compositional")
+
+
+
+#munida_core<-core_members(Munida_Comp, detection = 0.0, prevalence = 80/100,include.lowest = F)
+InPlace_Comp<-prune_taxa(rownames(InPlace_Comp@otu_table@.Data)[!taxa_sums(InPlace_Comp)==0],InPlace_Comp)
+rmeans_inplace<-data.frame(RMean=rowMeans(InPlace_Comp@otu_table@.Data))
+rmeans_inplace$Abundance_Class<-ifelse(rownames(rmeans_inplace) %in% inplace_core,"Core",
+                                       ifelse(rmeans_inplace$RMean > 0.001,"Abundant","Rare"))
+InPlace_Comp_DF<-as.data.frame(InPlace_Comp@otu_table@.Data)
+InPlace_Comp_DF$Group<-rmeans_inplace$Abundance_Class
+InPlaceTernarySums<-as.data.frame(t(rowsum((InPlace_Comp@otu_table@.Data), rmeans_inplace$Abundance_Class)))
+InPlaceTernarySums$SamplePop<-InPlace_Comp@sam_data$Location
+
+Munida_Comp<-prune_taxa(rownames(Munida_Comp@otu_table@.Data)[!taxa_sums(Munida_Comp)==0],Munida_Comp)
+rmeans_munida<-data.frame(RMean=rowMeans(Munida_Comp@otu_table@.Data))
+rmeans_munida$Abundance_Class<-ifelse(rownames(rmeans_munida) %in% inplace_core,"Core",
+                                      ifelse(rmeans_munida$RMean > 0.001,"Abundant","Rare"))
+munida_Comp_DF<-as.data.frame(Munida_Comp@otu_table@.Data)
+munida_Comp_DF$Group<-rmeans_munida$Abundance_Class
+munidaTernarySums<-as.data.frame(t(rowsum((Munida_Comp@otu_table@.Data), rmeans_munida$Abundance_Class)))
+munidaTernarySums$Sample<-rownames(munidaTernarySums)
+munidaTernarySums$Spec_Sample<-Munida_Comp@sam_data$Specific.Sample
+#munidaTernarySums$RaftTime<-RaftTimes_mins$RaftTime[match(munidaTernarySums$Spec_Sample,RaftTimes_mins$Sample)]
+#munidaTernarySums$RaftTime[is.na(munidaTernarySums$RaftTime)]<-0
+#plot(munidaTernarySums$Core~munidaTernarySums$RaftTime)
+
+
+InPlaceTernarySums$RaftType<-"Non-Raft"
+munidaTernarySums$RaftType<-"Raft"
+
+library(RColorBrewer)
+colfunc<-colorRampPalette(brewer.pal(8, "Blues"))
+yourScale <- c(colfunc(30))
+
+Ternary_Sums<-rbind(munidaTernarySums[c(1,2,3,6)],
+                    InPlaceTernarySums[c(1,2,3,5)])
+
+#Ternary_Sums$MeanSD<-rich_preds$MeanSD[match(rownames(Ternary_Sums),rownames(rich_preds))]
+#Ternary_Sums$RaftTime<-rich_preds$RaftTime[match(rownames(Ternary_Sums),rownames(rich_preds))]
+
+colors<-c("#5DBB63","#3281a8")
+library(ggtern)
+#set.seed(1)
+library(ggalt)
+TernaryPlot.pdf<-ggtern(data=Ternary_Sums,mapping=aes(Abundant, Core, Rare,col=RaftType)) + 
+  labs(title = "",
+       color ="Type of Sample") +
+  atomic_percent()   + #make ternary scales on atomic %
+  geom_point(size=4,shape=16)  + scale_colour_manual(values=colors) +
+  theme_rgbw() 
+TernaryPlot.pdf
+mean_terns<-as.data.frame(rbind(
+colMeans(Ternary_Sums[Ternary_Sums$RaftType=="Raft",1:3]),
+colMeans(Ternary_Sums[Ternary_Sums$RaftType=="Non-Raft",1:3])))
+mean_terns$RaftType<-c("Raft","Non-Raft")
+tern_sideplot<-ggtern(data=mean_terns,mapping=aes(Abundant, Core, Rare,col=RaftType)) + 
+  labs(title = "",
+       color ="Type of Sample") +
+  atomic_percent()   + #make ternary scales on atomic %
+  geom_point(size=4,shape=16)  + scale_colour_manual(values=colors) +
+  theme_rgbw() 
+ggsave(TernaryPlot.pdf,filename="TernaryPlot.pdf",height=5,width=5)
+ggsave(tern_sideplot,filename="tern_sideplot.pdf",height=5,width=5)
+
+tern_sideplot
+
+
+ggsave(TernaryPlot.pdf,filename="TernaryPlot.pdf",height=5,width=5)
+
+mean(Ternary_Sums$Abundant[Ternary_Sums$RaftType=="Raft"])
+mean(Ternary_Sums$Core[Ternary_Sums$RaftType=="Raft"])
+mean(Ternary_Sums$Rare[Ternary_Sums$RaftType=="Raft"])
+
+mean(Ternary_Sums$Abundant[Ternary_Sums$RaftType=="Non-Raft"])
+mean(Ternary_Sums$Core[Ternary_Sums$RaftType=="Non-Raft"])
+mean(Ternary_Sums$Rare[Ternary_Sums$RaftType=="Non-Raft"])
